3280c7ec2a6d387672134e70052450311b39be57
alexandreavg <40585436+alexandreavg@users.noreply.github.com>
2019-10-16 20:45:11 -0300

    Classes de Teste

---
+++ Servico/src/test/java/br/com/servico/service/GrupoServiceTeste.java
@@ -0,0 +1,43 @@
+package br.com.servico.service;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import br.com.servico.domain.Grupo;
+import br.com.servico.domain.Usuario;
+
+public class GrupoServiceTeste {
+	
+	@Test
+	public void gerarJson() {
+		Gson gson = new Gson();
+		
+		Grupo grupo = new Grupo();
+		Usuario moderador = new Usuario();
+		
+		moderador.setCurso("Ads");
+		moderador.setEmail("alexandre@gemail.com");
+		moderador.setNome("Alexandre");
+		moderador.setSemestre("6");
+		moderador.setSenha("abc123");
+		moderador.setSobreNome("V. Garcia");
+		
+		grupo.setNomeGrupo("Grupo para Teste");
+		grupo.setNomeMateria("Matematica");
+		grupo.setNotaGrupo(10.0);
+		grupo.setModerador(moderador);
+		
+		String mensagem = gson.toJson(grupo);
+		
+		System.out.println(mensagem);
+		
+		Grupo grupoQueVeioDoJson = new Grupo();
+		
+		grupoQueVeioDoJson = gson.fromJson(mensagem, Grupo.class);
+		
+		System.out.println(grupoQueVeioDoJson.getNomeGrupo());
+		
+	}
+	
+}
