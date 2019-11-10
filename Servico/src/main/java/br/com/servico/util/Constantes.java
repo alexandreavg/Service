b351ca107fc0614da7318b03fd008632f7cf6924
alexandreavg <40585436+alexandreavg@users.noreply.github.com>
2019-11-10 14:42:22 -0300

    Criptografia Melhorada

---
+++ Servico/src/main/java/br/com/servico/util/Constantes.java
@@ -0,0 +1,8 @@
+package br.com.servico.util;
+
+public class Constantes {
+	
+	public static String CHAVE_AES = "0123456789abcdef";
+	public static String TAMANHO_CHAVE = "AAAAAAAAAAAAAAAA";
+
+}
--- Servico/src/main/java/br/com/servico/util/CriptografiaUtil.java
+++ Servico/src/main/java/br/com/servico/util/CriptografiaUtil.java
@@ -8,21 +8,17 @@ import javax.crypto.Cipher;
 
 public class CriptografiaUtil {
 
-	static String IV = "AAAAAAAAAAAAAAAA";
-	static String textopuro = "Olá mundo! Esse é um texto que foi cifrado em AES 16 bit.";
-	static String chaveencriptacao = "0123456789abcdef";
-
-	public static byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+	public static byte[] encrypt(String textopuro) throws Exception {
 		Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
-		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
-		encripta.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		SecretKeySpec key = new SecretKeySpec(Constantes.CHAVE_AES.getBytes("UTF-8"), "AES");
+		encripta.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(Constantes.TAMANHO_CHAVE.getBytes("UTF-8")));
 		return encripta.doFinal(textopuro.getBytes("UTF-8"));
 	}
 
-	public static String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception {
+	public static String decrypt(byte[] textoencriptado) throws Exception {
 		Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
-		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
-		decripta.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		SecretKeySpec key = new SecretKeySpec(Constantes.CHAVE_AES.getBytes("UTF-8"), "AES");
+		decripta.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(Constantes.TAMANHO_CHAVE.getBytes("UTF-8")));
 		return new String(decripta.doFinal(textoencriptado), "UTF-8");
 	}
 
