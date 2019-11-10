fad609d7b3d72920ea7772a206722639c0661010
alexandreavg <40585436+alexandreavg@users.noreply.github.com>
2019-11-10 15:27:45 -0300

    Excluido classes nao usadas

--- Servico/src/main/java/br/com/servico/util/Constantes.java
+++
@@ -1,8 +0,0 @@
-package br.com.servico.util;
-
-public class Constantes {
-	
-	public static String CHAVE_AES = "0123456789abcdef";
-	public static String TAMANHO_CHAVE = "AAAAAAAAAAAAAAAA";
-
-}
--- Servico/src/main/java/br/com/servico/util/CriptografiaUtil.java
+++ Servico/src/main/java/br/com/servico/util/CriptografiaUtil.java
@@ -1,25 +1,7 @@
 package br.com.servico.util;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import javax.crypto.spec.IvParameterSpec;
-
-import javax.crypto.Cipher;
-
 public class CriptografiaUtil {
-
-	public static byte[] encrypt(String textopuro) throws Exception {
-		Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
-		SecretKeySpec key = new SecretKeySpec(Constantes.CHAVE_AES.getBytes("UTF-8"), "AES");
-		encripta.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(Constantes.TAMANHO_CHAVE.getBytes("UTF-8")));
-		return encripta.doFinal(textopuro.getBytes("UTF-8"));
-	}
-
-	public static String decrypt(byte[] textoencriptado) throws Exception {
-		Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
-		SecretKeySpec key = new SecretKeySpec(Constantes.CHAVE_AES.getBytes("UTF-8"), "AES");
-		decripta.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(Constantes.TAMANHO_CHAVE.getBytes("UTF-8")));
-		return new String(decripta.doFinal(textoencriptado), "UTF-8");
-	}
-
+	
+	
+	
 }
